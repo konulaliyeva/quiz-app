8237e0a8374c82fc376b4451ee3c6e185a821e64
Konul Aliyeva <konulaliyeva@Konuls-MacBook-Pro.local>
2022-07-18 16:02:12 +0400

    Adding the result of the questions

--- src/App.js
+++ src/App.js
@@ -8,48 +8,78 @@ class App extends React.Component {
     super(props);
     this.state = {
       score: 0,
-      div:null
+      div: null,
+      displayQuestions: true,
     };
-    this.handleFormSubmit = this.handleSubmitResult.bind(this);
+    this.handleSubmitResult = this.handleSubmitResult.bind(this);
     this.clickSubmitBtn = this.clickSubmitBtn.bind(this);
+    this.clickRetryBtn = this.clickRetryBtn.bind(this);
+    this.helloDiv = this.helloDiv.bind(this);
   }
 
   handleSubmitResult(selectedAnswer) {
     if (selectedAnswer.isCorrect) {
-      console.log(selectedAnswer.isCorrect)
-      this.setState((prevScore) => {
-        return {
-          newScore: prevScore+1,
-        };
+      this.setState((prevState) => {
+        return { score: prevState.score + 1 };
       });
+      // this.setState({ score: 4 });
     }
   }
-  
-  clickSubmitBtn(event){
+
+  helloDiv() {
+    return (
+      <div>
+        <h1>{this.state.score}</h1>
+      </div>
+    );
+  }
+
+  clickSubmitBtn(event) {
     this.setState({
-      div: "hello"
+      div: this.helloDiv(),
+      displayQuestions: !this.state.displayQuestions,
     });
-    }
-  
+    console.log(this.state);
+  }
+clickRetryBtn(){
+  this.setState({
+    displayQuestions: !this.state.displayQuestions,
+  })
+}
   render() {
-    
     return (
       <div className="container">
-        <h1 className="title">Quiz</h1>
-        <div className="questions">
-          {questions.map((question) => {
-            return (
-              <Question
-                key={question.id}
-                onResult={this.handleSubmitResult}
-                {...question}
-              />
-            );
-          })}
-        </div>
-        <div className="formSubmit">
-          <button onClick = {this.clickSubmitBtn} className="submit-btn">Submit</button>
-        </div>
+        {this.state.displayQuestions ? (
+          <>
+            <h1 className="title">Start to Take Quiz</h1>
+            <div className="questions">
+              {questions.map((question) => {
+                return (
+                  <Question
+                    key={question.id}
+                    onResult={this.handleSubmitResult}
+                    {...question}
+                  />
+                );
+              })}
+            </div>
+            <div className="formSubmit">
+              <button onClick={this.clickSubmitBtn} className="btn submit-btn">
+                Submit
+              </button>
+            </div>
+          </>
+        ) : (
+          <div className="result-container">
+            <h1>The Result of Your Quiz</h1>
+            <p> You scored {this.state.score} points out of 20 questions</p>
+            <div className="formSubmit">
+              <button onClick={this.clickRetryBtn} className="btn retry-btn">
+                Retry Quiz
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
